013022fa1dc4b52fa3283a305234926e6c1b4814
Durwankur Naik <ndurwankur14@gmail.com>
2023-11-29 11:56:25 +0530

    Changed Red emoji for Black one

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
       <footer className="flex flex-col md:flex-row items-center justify-between py-5 md:py-16 text-xs sm:text-base">
-        <span>Made with &#128420; by Durwankur S. Naik</span> {/* Heart emoji HTML code */}
+        <span>Made with &#128150; by Durwankur S. Naik</span> {/* Heart emoji HTML code */}
+        {/* &#128420 - Black Heart Emoji */}
   
         <span>© 2023 Durwankur | All rights reserved</span>
       </footer>
